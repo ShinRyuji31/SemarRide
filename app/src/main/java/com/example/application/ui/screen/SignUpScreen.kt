e9b849bc33f573fa4360ca96dbc139a4aac65408
Jeffierson T <jeffierson31@gmail.com>
2026-04-16 23:03:04 +0700

    repo name change trial1

--- app/src/main/java/com/example/application/ui/screen/SignUpScreen.kt
+++ app/src/main/java/com/example/application/ui/screen/SignUpScreen.kt
@@ -12,7 +12,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
@@ -20,20 +19,14 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.application.ui.component.ButtonBlue
-import com.example.application.ui.component.ButtonWhite
 import com.example.application.ui.component.TextFieldOutlineRegular
-import com.example.application.ui.theme.ApplicationTheme
 import com.example.application.ui.theme.blueWhiteGradient
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.foundation.clickable
 
 @Composable
